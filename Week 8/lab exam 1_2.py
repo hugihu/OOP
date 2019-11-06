42c07870bd2af13063fe8562e05e4ed2c1b8d57a
Hugh <c17316046@mydit.ie>
2019-11-06 20:10:58 +0000

    commit

--- Week 8/lab exam 1_2.py
+++ Week 8/lab exam 1_2.py
@@ -3,14 +3,12 @@
 # C17316046
 # Pascal's triangle
 
+# Appending old rows with new rows
 def make_new_row(old_row):
-
     if old_row == []:
         return [1]
     if old_row == [1]:
         return [1,1]
-
-    # part A
     new_row = [1]
     for i in range(len(old_row)-1):
         new_row.append(old_row[i] + old_row[i+1])
@@ -18,13 +16,13 @@ def make_new_row(old_row):
         new_row.append(1)
     return new_row
 
-x = 0
 n = 0
 
-while x == 0:
+# User interaction with program to request a certain number of rows
+while True:
     user_row = input("Please enter how many rows you would like(Greater than 2):")
     user_row = int(user_row)
-    if user_row <  2:
+    if user_row < 2:
         print("Please enter a valid number of rows greater than 2 ")
         continue
     elif user_row == 2:
@@ -35,6 +33,7 @@ while x == 0:
         n = int(user_row)
         break
 
+# Calling functions to make rows and compile them as a list
 L = [[1]]
 r = [1,1]
 for i in range(n-2):
@@ -43,14 +42,16 @@ for i in range(n-2):
 else:
     L.append(r)
 
+# Printing output as a list
 print("Printing whole list of lists:")
 print(L)
 
+# Printing each row on individual lines
 print("Printing list of lists, one list at a time:")
 for i in L:
     print(i)
 
-# Printing triangle as string
+# Printing rows on individual lines centered
 print("Printing lists as strings")
 for i in L:
     row = ' '.join([str(j) for j in i])
